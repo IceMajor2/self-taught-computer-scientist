659fe0612e95fa6e35cb13ba02822b781e41943b
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-23 14:55:34 +0200

    Rename class + implement simple linear search alghoritm with generics

---
+++ search-03/src/main/java/LinearSearch.java
@@ -0,0 +1,18 @@
+public class LinearSearch {
+
+	/**
+	 * Call this method to search for a desired element.
+	 * Implementation: <b>linear search</b>.
+	 * @param array data set as array
+	 * @param object element to look for
+	 * @return {@code object} if it was found, {@code null} if not
+	 */
+	public static <T> T get(T[] array, T object) {
+		for(T element : array) {
+			if(element.equals(object)) {
+				return object;
+			}
+		}
+		return null;
+	}
+}
