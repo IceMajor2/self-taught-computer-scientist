584daf7e38a77aa86f25366065bf0c051aaa0705
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-23 15:24:04 +0200

    Write new tests

--- search-03/src/test/java/LinearSearchTest.java
+++ search-03/src/test/java/LinearSearchTest.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +15,46 @@ class LinearSearchTest {
 				"Grzegorz", "Natalia", "Jerzy", "Jacek" };
 		String found = LinearSearch.get(strings, "Jacek");
 		assertNotNull(found);
-		assertEquals(found, "Jacek");
+		assertEquals("Jacek", found);
+	}
+
+	@Test
+	void getInteger() {
+		Integer[] numbers = new Integer[] {-1, -5, 2, 123, -9, 0, 8943, 321, 832, 59423, 12, 13};
+		Integer found = LinearSearch.get(numbers, -5);
+		assertNotNull(found);
+		assertEquals(-5, found);
+	}
+
+	@Test
+	void getInteger_whenNotInArrayTest() {
+		Integer[] numbers = new Integer[] {-1, -5, 2, 123, -9, 0, 8943, 321, 832, 59423, 12, 13};
+		Integer found = LinearSearch.get(numbers, -3);
+		assertNull(found);
+	}
+
+	@Test
+	void getFloatFromBigDataSetTest() {
+		Float[] floats = getBigFloatArray(10000000);
+		Float find = floats[floats.length - 1];
+		Float found = LinearSearch.get(floats, find);
+		assertNotNull(found);
+		assertEquals(find, found);
+	}
+
+	private Float[] getBigFloatArray(int size) {
+		Random random = new Random();
+		Set<Float> set = new HashSet<>();
+		Float[] floats = new Float[size];
+
+		int i = 0;
+		while(set.size() < size) {
+			float number = random.nextFloat(1000) - 500;
+			if(set.add(number)) {
+				floats[i] = number;
+				i++;
+			}
+		}
+		return floats;
 	}
 }
