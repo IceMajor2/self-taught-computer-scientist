3970a619ad635857385647a12ee8463add3a0687
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-23 18:57:24 +0200

    Write binary search with generics

--- search-03/src/main/java/BinarySearch.java
+++ search-03/src/main/java/BinarySearch.java
@@ -1,49 +1,34 @@
-
+import java.util.Comparator;
 
 public class BinarySearch {
 
-	public static void main(String[] args) {
-		int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-		int[] reverted = new int[] { 8, 7, 6, 5, 4, 3, 2, 1 };
-		for (int i = 7; i >= 0; i--) {
-			System.out.println("Znajdź %d-kę".formatted(array[i]));
-			System.out.println("Znalazłem. Indeks szukanej liczby to: %d"
-					.formatted(get(array, array[i])));
-		}
-
-		for (int i = 7; i >= 0; i--) {
-			System.out.println("Znajdź %d-kę".formatted(reverted[i]));
-			System.out.println("Znalazłem. Indeks szukanej liczby to: %d"
-					.formatted(get(reverted, reverted[i])));
-		}
-	}
-
-	public static int get(int[] array, int searchFor) {
-		int monotonicity = determineMonotonicity(array);
+	public static <T> int get(T[] array, T searchFor, Comparator<T> comparator) {
+		int monotonicity = determineMonotonicity(array, comparator);
 		int left = 0;
 		int right = array.length - 1;
 		int middle = (left + right) / 2;
 
-		int currElement = array[middle];
-		while (currElement != searchFor) {
-			if (currElement > searchFor) {
+		T currElement = array[middle];
+		int result = comparator.compare(currElement, searchFor);
+
+		while (result != 0) {
+			if (result > 0) {
 				if (monotonicity == 1) right = middle - 1;
 				else left = middle + 1;
 				middle = (left + right) / 2;
 			}
-			if (currElement < searchFor) {
+			if (result < 0) {
 				if (monotonicity == 1) left = middle + 1;
 				else right = middle - 1;
 				middle = (left + right) / 2;
 			}
 			currElement = array[middle];
+			result = comparator.compare(currElement, searchFor);
 		}
 		return middle;
 	}
 
-	private static int determineMonotonicity(int[] array) {
-		if (array[0] < array[array.length - 1]) return 1;
-		if (array[0] > array[array.length - 1]) return -1;
-		return 0;
+	private static <T> int determineMonotonicity(T[] array, Comparator<T> comparator) {
+		return comparator.compare(array[array.length - 1], array[0]);
 	}
 }
