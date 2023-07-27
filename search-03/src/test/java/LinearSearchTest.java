b77d5fada6e9459a4952db253cbd029e5597095a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-27 11:43:57 +0200

    Parameterize further

--- search-03/src/test/java/LinearSearchTest.java
+++ search-03/src/test/java/LinearSearchTest.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.Collections;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 class LinearSearchTest {
 
 	@ParameterizedTest
-	@ValueSource(ints = {0, 10, 3, 19})
+	@ValueSource(ints = { 0, 10, 3, 19 })
 	void findStringTest(int index) {
 		String[] strings = TestUtils.getNames(20);
 		String toFind = strings[index];
@@ -22,7 +21,7 @@ class LinearSearchTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = {60, 2, 73, 22, 0, 74})
+	@ValueSource(ints = { 60, 2, 73, 22, 0, 74 })
 	void findIntegerTest(int index) {
 		Integer[] numbers = TestUtils.getNumbersFromTo(-5, 70);
 		Collections.shuffle(Arrays.asList(numbers));
@@ -32,22 +31,23 @@ class LinearSearchTest {
 		assertEquals(toFind, found);
 	}
 
-	@Test
-	void findIntegerNotInArrayTest() {
-		Integer[] numbers = new Integer[] { -1, -5, 2, 123, -9, 0, 8943, 321, 832, 59423, 12, 13 };
-		Integer found = LinearSearch.get(numbers, -3);
+	@ParameterizedTest
+	@ValueSource(ints = { 9, -5, -2 })
+	void findIntegerNotInArrayTest(int toFind) {
+		Integer[] numbers = TestUtils.getNumbersFromTo(-1, 8);
+		Integer found = LinearSearch.get(numbers, toFind);
 		assertNull(found);
 	}
 
-	@Test
-	void findIntegersInBigDataSetTest() {
-//		Integer[] ints = TestUtils.getUniqueRandomArray(100000);
-//		for (int i = 0; i < ints.length; i += 1) {
-//			Integer find = ints[ints.length - 1];
-//			Integer found = LinearSearch.get(ints, find);
-//			assertNotNull(found);
-//			assertEquals(find, found);
-//		}
+	@ParameterizedTest
+	@ValueSource(ints = { 0, 5821, 92100, 10, 299999, 111111 })
+	void findIntegersInBigDataSetTest(int index) {
+		Integer[] numbers = TestUtils.getNumbersFromTo(-250000, 50000);
+		Collections.shuffle(Arrays.asList(numbers));
+		Integer toFind = numbers[index];
+		Integer found = LinearSearch.get(numbers, toFind);
+		assertNotNull(found);
+		assertEquals(toFind, found);
 	}
 
 	/**
