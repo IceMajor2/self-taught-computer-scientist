d1eaf6af8d45638e9b6497d2613b0ce57baa40d7
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-27 11:09:24 +0200

    Write a method that returns a shuffled array of names + provide a .txt file with unique first names

--- search-03/src/test/java/TestUtils.java
+++ search-03/src/test/java/TestUtils.java
@@ -1,5 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
 public class TestUtils {
@@ -10,14 +17,36 @@ public class TestUtils {
 
 		Random random = new Random();
 		int i = 0;
-		while(set.size() != size) {
+		while (set.size() != size) {
 			int range = Integer.MAX_VALUE;
 			int num = random.nextInt(range) - range / 2;
-			if(set.add(num)) {
+			if (set.add(num)) {
 				array[i] = num;
 				i++;
 			}
 		}
 		return array;
+	}
+
+	public static String[] getNames(int howMany) {
+		File namesTxt = new File(Paths.get("src\\test\\resources\\first-names.txt").toUri());
+
+		try (Scanner scanner = new Scanner(namesTxt)) {
+
+			List<String> names = new ArrayList<>();
+			int index = 0;
+			while (scanner.hasNext() && index < howMany) {
+				String nextName = scanner.next();
+				names.add(nextName);
+				index++;
+			}
+
+			Collections.shuffle(names);
+			String[] strarray = names.toArray(new String[0]);
+			return strarray;
+		}
+		catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
