a0a251113aca11ede77ebbef3d04aa3f5d7f081c
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 11:44:33 +0200

    Fix csv conversion + expand test (new parameters)

--- sort-04/src/test/java/MergeSortTest.java
+++ sort-04/src/test/java/MergeSortTest.java
@@ -1,17 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MergeSortTest {
 
 	@ParameterizedTest
 	@CsvSource({
-			"[0;9;-10;5;-6],[-10;-6;0;5;9]"
+			"[0;9;-10;5;-6],[-10;-6;0;5;9]",
+			"[-1;2;0;4],[-1;0;2;4]",
+			"[-15;-100;5239;91;-20;1;2],[-100;-20;-15;1;2;91;5239]"
 	})
 	void shouldReturnSortedArrayTest(Object array, Object expected) {
 		Integer[] input = parseCsvSourceToIntArray(array.toString());
@@ -26,16 +24,14 @@ class MergeSortTest {
 	}
 
 	private Integer[] parseCsvSourceToIntArray(String csvSource) {
-		List<Integer> list = new ArrayList<>();
-		for (int i = csvSource.indexOf('[') + 1; i < csvSource.lastIndexOf(']'); i++) {
-			char charAt = csvSource.charAt(i);
-			if (charAt == ';') {
-				continue;
-			}
-			list.add(Integer.valueOf(charAt));
+		csvSource = csvSource.replace("[", "").replace("]", "");
+		String[] strNums = csvSource.split(";");
+		Integer[] intArray = new Integer[strNums.length];
+		int index = 0;
+		for(String strNum :strNums) {
+			intArray[index] = Integer.valueOf(strNum);
+			index++;
 		}
-		Integer[] intArray = new Integer[list.size()];
-		intArray = list.toArray(intArray);
 		return intArray;
 	}
 }
