f9993ae6fc109e84a384036b2b2596b8f978c6d5
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-07-31 11:44:48 +0200

    Write merge sort alghoritm

--- sort-04/src/main/java/MergeSort.java
+++ sort-04/src/main/java/MergeSort.java
@@ -1,6 +1,58 @@
+import java.util.Arrays;
+
 public class MergeSort {
 
+	public static void main(String[] args) {
+		Integer[] array = new Integer[] { 7, 1, 9, 8 };
+		Integer[] sorted = sort(array);
+		System.out.println(Arrays.toString(sorted));
+	}
+
 	public static Integer[] sort(Integer[] input) {
-		return null;
+		if (input.length > 1) {
+			int midIndex = input.length / 2;
+			Integer[] leftHalf = createLeftHalfArray(input, midIndex);
+			Integer[] rightHalf = createRightHalfArray(input, midIndex);
+			sort(leftHalf);
+			sort(rightHalf);
+
+			int leftIndex = 0;
+			int rightIndex = 0;
+			int sortedIndex = 0;
+			while (leftIndex < leftHalf.length && rightIndex < rightHalf.length) {
+				if (leftHalf[leftIndex] <= rightHalf[rightIndex]) {
+					input[sortedIndex] = leftHalf[leftIndex];
+					leftIndex++;
+				}
+				else {
+					input[sortedIndex] = rightHalf[rightIndex];
+					rightIndex++;
+				}
+				sortedIndex++;
+			}
+			while (leftIndex < leftHalf.length) {
+				input[sortedIndex] = leftHalf[leftIndex];
+				leftIndex++;
+				sortedIndex++;
+			}
+			while (rightIndex < rightHalf.length) {
+				input[sortedIndex] = rightHalf[rightIndex];
+				rightIndex++;
+				sortedIndex++;
+			}
+		}
+		return input;
+	}
+
+	private static Integer[] createRightHalfArray(Integer[] input, int midIndex) {
+		Integer[] rightHalf = new Integer[input.length - midIndex];
+		System.arraycopy(input, midIndex, rightHalf, 0, input.length - midIndex);
+		return rightHalf;
+	}
+
+	private static Integer[] createLeftHalfArray(Integer[] input, int midIndex) {
+		Integer[] leftHalf = new Integer[midIndex];
+		System.arraycopy(input, 0, leftHalf, 0, midIndex);
+		return leftHalf;
 	}
 }
