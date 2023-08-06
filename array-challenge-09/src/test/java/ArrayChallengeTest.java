3813e369cf9c5be76f633383600a7b300975b607
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-06 14:57:16 +0200

    Write simplest test case

--- array-challenge-09/src/test/java/ArrayChallengeTest.java
+++ array-challenge-09/src/test/java/ArrayChallengeTest.java
@@ -1,11 +1,22 @@
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import utils.TestUtils;
+
+import static org.assertj.core.api.Assertions.*;
 
 class ArrayChallengeTest {
 
-	@Test
-	void sort() {
-
+	@ParameterizedTest
+	@CsvSource({
+			"[1, 0, 9, 15, 109, 12, 15], [0, 12, 1, 9, 15, 109, 15]",
+			"[11, 16, 7, 9, 98, 68, 33], [16, 98, 68, 11, 7, 9, 33]",
+			"[20, 24, 32, 19, 22, 100, 1], [20, 24, 32, 22, 100, 22, 1]"
+	})
+	void sortTest(String inputStr, String expectedStr) {
+		int[] input = TestUtils.parseCsvSourceToIntArray(inputStr);
+		int[] expected = TestUtils.parseCsvSourceToIntArray(expectedStr);
+		int[] actual = ArrayChallenge.sort(input);
+		assertThat(actual).containsExactly(expected);
 	}
 }
