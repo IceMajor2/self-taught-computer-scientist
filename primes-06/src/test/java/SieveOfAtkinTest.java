a484adc5adac65b67984d3b960d05135bcfda6fc
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-06 12:34:14 +0200

    Write test case

---
+++ primes-06/src/test/java/SieveOfAtkinTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SieveOfAtkinTest {
+
+	@Test
+	void findPrimes() {
+		int[] expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+				53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+		int[] actual = SieveOfAtkin.findPrimes(100);
+
+		assertThat(actual)
+				.withFailMessage("Output was null")
+				.isNotNull()
+				.withFailMessage("Output is not sorted in ascending order")
+				.isSorted()
+				.withFailMessage("Output does not match expected numbers")
+				.containsExactly(expected);
+	}
+
+	// edge cases: n is less than 0; n = 0, n = 1, n = 2
+}
