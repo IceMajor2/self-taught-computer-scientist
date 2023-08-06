d4045d625936cb99f23b7926baf2f813646f04d0
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-06 17:55:38 +0200

    Improve parameters (and make a fix)

--- array-challenge-09/src/test/java/ArrayChallengeTest.java
+++ array-challenge-09/src/test/java/ArrayChallengeTest.java
@@ -8,10 +8,11 @@ import static org.assertj.core.api.Assertions.*;
 class ArrayChallengeTest {
 
 	@ParameterizedTest
-	@CsvSource({
-			"[1, 0, 9, 15, 109, 12, 15], [0, 12, 1, 9, 15, 109, 15]",
-			"[11, 16, 7, 9, 98, 68, 33], [16, 98, 68, 11, 7, 9, 33]",
-			"[20, 24, 32, 19, 22, 100, 1], [20, 24, 32, 22, 100, 22, 1]"
+	@CsvSource(value = {
+			"[1;0;9;15;109;12;15],[0;12;1;9;15;109;15]",
+			"[11;16;7;9;98;68;33],[16;98;68;11;7;9;33]",
+			"[20;24;32;19;22;100;1],[20;24;32;22;100;19;1]",
+			"[21;24;7;4;19;0;11;300;15;12],[24;4;0;300;12;21;7;19;11;15]"
 	})
 	void sortTest(String inputStr, String expectedStr) {
 		int[] input = TestUtils.parseCsvSourceToIntArray(inputStr);
