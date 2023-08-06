c2141e902a01416a95466876352aed9039384ffb
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-06 14:13:21 +0200

    Add big data test

--- primes-06/src/test/java/SieveOfAtkinTest.java
+++ primes-06/src/test/java/SieveOfAtkinTest.java
@@ -1,5 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,7 +20,7 @@ class SieveOfAtkinTest {
 	private static final int[] PRIMES_7 = new int[] { 2, 3, 5, 7 };
 
 	@Test
-	// TODO: split assertions into other test cases
+		// TODO: split assertions into other test cases
 	void findPrimesTest() {
 		int[] expected = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
 				53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
@@ -40,12 +47,38 @@ class SieveOfAtkinTest {
 	void findPrimes_whenNisNotBiggerThan7Test(int n) {
 		List<Integer> primeTemp = new ArrayList<>();
 		for (int prime : PRIMES_7) {
-			if(prime <= n) {
+			if (prime <= n) {
 				primeTemp.add(prime);
 			}
 		}
 		int[] expected = primeTemp.stream().mapToInt(i -> i).toArray();
 		int[] actual = SieveOfAtkin.findPrimes(n);
 		assertThat(actual).containsExactly(expected);
+	}
+
+	@Test
+	void findPrimesBigDataTest() {
+		int[] expected = readPrimesFile();
+		int[] actual = SieveOfAtkin.findPrimes(1299827);
+		assertThat(actual).containsExactly(expected);
+	}
+
+	private int[] readPrimesFile() {
+		String filename = "primes_100_008.txt";
+		int[] array = new int[100008];
+		File primesTxt = new File(Paths.get("src\\test\\resources\\" + filename).toUri());
+		try (Scanner scanner = new Scanner(primesTxt)) {
+			int index = 0;
+			while (scanner.hasNextInt()) {
+				int num = scanner.nextInt();
+				array[index] = num;
+				index++;
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		Logger.getAnonymousLogger().log(Level.INFO, "Done loading '" + filename + "'");
+		return array;
 	}
 }
