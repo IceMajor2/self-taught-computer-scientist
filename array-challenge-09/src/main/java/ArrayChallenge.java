2e31e325562268fb2ba1e890193ed3ac1f17e6f6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-06 17:55:49 +0200

    Implement sort algorithm

--- array-challenge-09/src/main/java/ArrayChallenge.java
+++ array-challenge-09/src/main/java/ArrayChallenge.java
@@ -4,6 +4,38 @@
 public class ArrayChallenge {
 
 	public static int[] sort(int[] array) {
-		return null;
+		int[] sorted = new int[array.length];
+		int right = array.length - 1;
+		int left = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (isEven(array[i])) {
+				sorted[left] = array[i];
+				left++;
+			}
+			else {
+				sorted[right] = array[i];
+				right--;
+			}
+		}
+		partReverse(sorted, right + 1, array.length - 1);
+		return sorted;
+	}
+
+	private static boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+
+	private static void partReverse(int[] array, int from, int to) {
+		while (from < to) {
+			swap(array, to, from);
+			to--;
+			from++;
+		}
+	}
+
+	private static void swap(int[] array, int index1, int index2) {
+		int helper = array[index1];
+		array[index1] = array[index2];
+		array[index2] = helper;
 	}
 }
