ab69323d6b3688c94ff645d2d1542e488c1aefee
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-06 13:19:33 +0200

    Write edge test case

--- primes-06/src/test/java/SieveOfAtkinTest.java
+++ primes-06/src/test/java/SieveOfAtkinTest.java
@@ -1,13 +1,14 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class SieveOfAtkinTest {
 
 	@Test
-	void findPrimes() {
-		int[] expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
-				53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+	void findPrimesTest() {
+		int[] expected = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+				53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
 		int[] actual = SieveOfAtkin.findPrimes(100);
 
 		assertThat(actual)
@@ -20,4 +21,9 @@ class SieveOfAtkinTest {
 	}
 
 	// edge cases: n is less than 0; n = 0, n = 1, n = 2
+	@Test
+	void findPrimes_whenNis0Test() {
+		assertThatExceptionOfType(IllegalArgumentException.class)
+				.isThrownBy(() -> SieveOfAtkin.findPrimes(0));
+	}
 }
