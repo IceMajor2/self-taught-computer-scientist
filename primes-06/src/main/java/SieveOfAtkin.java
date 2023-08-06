942e34a26b630139c4fc3a913de10efcdf70763d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-06 13:21:13 +0200

    Providing non-positive numbers to 'findPrimes' method will throw exception

--- primes-06/src/main/java/SieveOfAtkin.java
+++ primes-06/src/main/java/SieveOfAtkin.java
@@ -1,7 +1,7 @@
 public class SieveOfAtkin {
 
 	public static int[] findPrimes(int n) {
-		if(n == 0) {
+		if(n <= 0) {
 			throw new IllegalArgumentException("The parameter must be a positive number");
 		}
 		boolean[] markedPrimes = calculatePrimes(n);
