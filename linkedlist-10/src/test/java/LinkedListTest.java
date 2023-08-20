6e064a3deecb072e877000170a64f973f8719077
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-20 10:53:58 +0200

    Test: on array index out of bounds expect exception

--- linkedlist-10/src/test/java/LinkedListTest.java
+++ linkedlist-10/src/test/java/LinkedListTest.java
@@ -13,20 +13,22 @@ import static org.assertj.core.api.Assertions.*;
 class LinkedListTest {
 
 	private LinkedList<Integer> emptyLinkedList;
+
 	private LinkedList<Integer> linkedListWithNumbers;
+
 	// 12 items
-	private int[] testNumbers = {10, -5, 0, 19, 21, -24, 222, -194, -1, 120, 9230, 102};
+	private int[] testNumbers = { 10, -5, 0, 19, 21, -24, 222, -194, -1, 120, 9230, 102 };
 
 	@BeforeEach
 	public void setup() {
 		this.emptyLinkedList = new LinkedList<>();
 		this.linkedListWithNumbers = new LinkedList<>();
-		for(int num : testNumbers) linkedListWithNumbers.add(num);
+		for (int num : testNumbers) linkedListWithNumbers.add(num);
 	}
 
 	@Test
 	void addTest() {
-		for(int num : testNumbers) emptyLinkedList.add(num);
+		for (int num : testNumbers) emptyLinkedList.add(num);
 		assertThat(emptyLinkedList.size())
 				.isEqualTo(testNumbers.length);
 	}
@@ -34,7 +36,7 @@ class LinkedListTest {
 	@Test
 	void addFirstTest() {
 		// when
-		for(int num : testNumbers) emptyLinkedList.addFirst(num);
+		for (int num : testNumbers) emptyLinkedList.addFirst(num);
 		// then
 		List<Integer> expected = convertIntArrayToList(testNumbers);
 		Collections.reverse(expected);
@@ -45,9 +47,23 @@ class LinkedListTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = {5, 7, 11, 0, 2, 3, 4, 1, 8, 6, 9, 10})
+	@ValueSource(ints = { 5, 7, 11, 0, 2, 3, 4, 1, 8, 6, 9, 10 })
 	void getIndexTest(int index) {
 		assertThat(linkedListWithNumbers.get(index)).isEqualTo(testNumbers[index]);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { -1, -5, -523498, 12, 13, 12409 })
+	void getIndexOutOfBoundsTest(int index) {
+		assertThatExceptionOfType(ArrayIndexOutOfBoundsException.class)
+				.isThrownBy(() -> linkedListWithNumbers.get(index));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { -1, 0, 1, 2, 3, 4, 491, -510})
+	void getIndexWhenListEmptyTest(int index) {
+		assertThatExceptionOfType(ArrayIndexOutOfBoundsException.class)
+				.isThrownBy(() -> emptyLinkedList.get(index));
 	}
 
 	@Test
