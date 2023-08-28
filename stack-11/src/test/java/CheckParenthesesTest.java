11caf35033c0901829e85acc0d765a771080c559
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-28 14:02:37 +0200

    Write test checking for expected 'false' value + make the tests parameterized

--- stack-11/src/test/java/CheckParenthesesTest.java
+++ stack-11/src/test/java/CheckParenthesesTest.java
@@ -1,12 +1,19 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CheckParenthesesTest {
 
-    @Test
-    void checkParenthesesTest() {
-        String parentheses = "(())()()";
+    @ParameterizedTest
+    @ValueSource(strings = {"(())()()", "( )( )()", "() () (( (())))", " () ", "{}({})", "{)"})
+    void checkParenthesesCorrectTest(String parentheses) {
         assertThat(CheckParentheses.parenthesesBalance(parentheses)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"())(", "}{}{", "( ( )))", "){))", "{}{ { ){))"})
+    void checkParenthesesWrongTest(String parentheses) {
+        assertThat(CheckParentheses.parenthesesBalance(parentheses)).isFalse();
     }
 }
