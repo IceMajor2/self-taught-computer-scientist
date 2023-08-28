5e693f9395b307eedd7f69accc223d51f9745ed5
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-28 13:52:35 +0200

    Implement linked lists that track min and max values. Retrieve time: O(1)

--- stack-11/src/main/java/LinkedStack.java
+++ stack-11/src/main/java/LinkedStack.java
@@ -6,72 +6,108 @@ import linkedlist.LinkedList;
 
 public class LinkedStack<T> implements MyCollection<T> {
 
-	private LinkedList<T> linkedList = new LinkedList<>();
+    private LinkedList<T> maxStack = new LinkedList<>();
+    private LinkedList<T> minStack = new LinkedList<>();
+    private LinkedList<T> linkedList = new LinkedList<>();
 
-	public void push(T object) {
-		this.linkedList.add(object);
-	}
+    public void push(T object) {
+        pushMin(object);
+        pushMax(object);
+        this.linkedList.add(object);
+    }
 
-	public T pop() {
-		return this.linkedList.removeLast();
-	}
+    private void pushMax(T object) {
+        if (object instanceof Number) {
+            if (size() == 0) {
+                maxStack.add(object);
+                return;
+            }
+            double newNumber = ((Number) object).doubleValue();
+            double previousNumber = ((Number) maxStack.getLast()).doubleValue();
+            if (newNumber >= previousNumber) maxStack.add(object);
+            else maxStack.add(maxStack.getLast());
+        }
+    }
 
-	@Override
-	public void add(T object) {
-		push(object);
-	}
+    private void pushMin(T object) {
+        if (object instanceof Number) {
+            if (size() == 0) {
+                minStack.add(object);
+                return;
+            }
+            double newNumber = ((Number) object).doubleValue();
+            double prevNumber = ((Number) minStack.getLast()).doubleValue();
+            if (newNumber <= prevNumber) minStack.add(object);
+            else minStack.add(minStack.getLast());
+        }
+    }
 
-	@Override
-	public T get() {
-		return linkedList.getLast();
-	}
+    public T pop() {
+        this.maxStack.removeLast();
+        this.minStack.removeLast();
+        return this.linkedList.removeLast();
+    }
 
-	@Override
-	public boolean contains(Object object) {
-		return linkedList.contains(object);
-	}
+    @Override
+    public void add(T object) {
+        push(object);
+    }
 
-	@Override
-	public boolean containsAll(Collection<?> collection) {
-		return linkedList.containsAll(collection);
-	}
+    @Override
+    public T get() {
+        return linkedList.getLast();
+    }
 
-	@Override
-	public T remove() {
-		return pop();
-	}
+    @Override
+    public boolean contains(Object object) {
+        return linkedList.contains(object);
+    }
 
-	@Override
-	public int size() {
-		return linkedList.size();
-	}
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return linkedList.containsAll(collection);
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return linkedList.isEmpty();
-	}
+    @Override
+    public T remove() {
+        return pop();
+    }
 
-	@Override
-	public void clear() {
-		linkedList.clear();
-	}
+    @Override
+    public int size() {
+        return linkedList.size();
+    }
 
-	@Override
-	public List<T> toList() {
-		return linkedList.toList();
-	}
+    @Override
+    public boolean isEmpty() {
+        return linkedList.isEmpty();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		return linkedList.equals(obj);
-	}
+    @Override
+    public void clear() {
+        linkedList.clear();
+    }
 
-	@Override
-	public String toString() {
-		return linkedList.toString();
-	}
+    @Override
+    public List<T> toList() {
+        return linkedList.toList();
+    }
 
-	public T max() {
-		return null;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        return linkedList.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return linkedList.toString();
+    }
+
+    public T max() {
+        return this.maxStack.getLast();
+    }
+
+    public T min() {
+        return this.minStack.getLast();
+    }
 }
