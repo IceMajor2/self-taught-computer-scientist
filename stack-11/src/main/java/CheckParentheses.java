8bf43b5b5bd5b72c29c9cdcd7c0f387edd1aa15a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-08-28 14:28:49 +0200

    Implement parentheses checking

--- stack-11/src/main/java/CheckParentheses.java
+++ stack-11/src/main/java/CheckParentheses.java
@@ -1,6 +1,35 @@
 public class CheckParentheses {
 
+    private static final char[] OPENING_PARENTHESES = {'[', '{', '('};
+    private static final char[] CLOSING_PARENTHESES = {']', '}', ')'};
+
     public static boolean parenthesesBalance(String parentheses) {
-        return true;
+        LinkedStack<Character> stack = new LinkedStack<>();
+        for (char ch : parentheses.toCharArray()) {
+            if (Character.isWhitespace(ch)) continue;
+            if (isOpeningParenthesis(ch)) {
+                stack.push(ch);
+            } else if (isClosingParenthesis(ch)) {
+                if (stack.size() == 0) return false;
+                else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.size() == 0;
+    }
+
+    private static boolean isClosingParenthesis(char ch) {
+        for (char closing : CLOSING_PARENTHESES) {
+            if (ch == closing) return true;
+        }
+        return false;
+    }
+
+    private static boolean isOpeningParenthesis(char ch) {
+        for (char opening : OPENING_PARENTHESES) {
+            if (ch == opening) return true;
+        }
+        return false;
     }
 }
