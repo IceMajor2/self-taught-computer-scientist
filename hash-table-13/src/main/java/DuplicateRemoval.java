a5b7ba7f5838ad2498fcde1253a076c8b708291d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-01 13:41:10 +0200

    Write code that passes previously written test

--- hash-table-13/src/main/java/DuplicateRemoval.java
+++ hash-table-13/src/main/java/DuplicateRemoval.java
@@ -1,6 +1,49 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class DuplicateRemoval {
 
+    private static List<Character> excludedChars = List.of
+            ('!', '?', '{', '}', '[', ']', '(', ')', ',', '.', '-', ':', ';');
+
+    /**
+     * Passing a text with duplicates into this function will return
+     * a {@code String} where each word is appearing at most once.
+     */
     public static String removeDuplicates(String str) {
-        return str;
+        Map<String, Integer> wordCount = new HashMap<>();
+        Scanner scanner = new Scanner(str);
+
+        StringBuilder noDuplicates = new StringBuilder("");
+        while (scanner.hasNext()) {
+            String next = scanner.next();
+            String pureWord = getPureWord(next);
+            if (wordCount.containsKey(pureWord)) {
+                continue;
+            }
+            wordCount.put(pureWord, 1);
+            noDuplicates.append(next + " ");
+        }
+        return noDuplicates.deleteCharAt(noDuplicates.length() - 1).toString();
+    }
+
+    /**
+     * Returns a 'distilled' form of string provided on input.
+     * Distilled, in this context, means a pure, dictionary word i.e.
+     * without commas, parentheses, in lowercase, etc.
+     * Characters that get removed: ['!', '?', '{', '}', '[', ']', '(', ')', ',', '.', '-', ':', ';']
+     */
+    private static String getPureWord(String word) {
+        StringBuilder sb = new StringBuilder(word.toLowerCase());
+        if (excludedChars.contains(word.charAt(0))) {
+            sb.deleteCharAt(0);
+        }
+        int lastIndex = word.length() - 1;
+        if (excludedChars.contains(word.charAt(lastIndex))) {
+            sb.deleteCharAt(lastIndex);
+        }
+        return sb.toString();
     }
 }
