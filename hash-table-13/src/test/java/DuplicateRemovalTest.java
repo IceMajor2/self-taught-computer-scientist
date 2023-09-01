0745b61ec2bfb3a9ca92de649b8c18026aebd606
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-09-01 13:23:38 +0200

    Fix test: change delimiter from ',' to '|'

--- hash-table-13/src/test/java/DuplicateRemovalTest.java
+++ hash-table-13/src/test/java/DuplicateRemovalTest.java
@@ -1,19 +1,18 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DuplicateRemovalTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "being book bad breaking being break bad, being book bad breaking break",
-            "joey steven michael george george george mike, joey steven michael george mike",
-            "They're playing the piano while flying in the plane, They're playing the piano while flying in plane",
-            "You can provide only 1 parameter with Value Source, but you can have multiple parameters with Csv Source, " +
+    @CsvSource(value = {
+            "being book bad breaking being break bad | being book bad breaking break",
+            "joey steven michael george george george mike | joey steven michael george mike",
+            "They're playing the piano while flying in the plane | They're playing the piano while flying in plane",
+            "You can provide only 1 parameter with Value Source, but you can have multiple parameters with Csv Source | " +
                     "You can provide only 1 parameter with Value Source, but have multiple parameters Csv"
-    })
+    }, delimiter = '|')
     void shouldReturnDuplicateWordsFromString(String str, String expected) {
         assertThat(DuplicateRemoval.removeDuplicates(str)).isEqualTo(expected);
     }
